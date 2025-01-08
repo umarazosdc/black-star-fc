76b82e48552933231ae1ad21bc5fca28afe591d7
umarazosdc <malamisa360@gmail.com>
2025-01-07 19:54:29 -0800

    Retouched the about user page

--- app/admin/dashboard/user/page.tsx
+++ app/admin/dashboard/user/page.tsx
@@ -5,11 +5,14 @@ import React from 'react';
 const UserAboutPage = () => {
    return (
       <div className="absolute inset-0 w-full h-full overflow-auto">
-         {/* Use a wrapper to reset padding and ensure proper layout */}
          <div className="-mx-4 relative w-full h-full">
-            <UserAboutContent className="relative top-40 p-8 pt-12 border-t" />
+            <UserAboutContent className="relative top-40 pl-8 pt-12 border-t" />
             <div className="size-20 absolute top-28 left-8">
-               <Img src="/imgs/users/scout/dc2.jpg" alt="Scout" className='rounded-full border'/>
+               <Img
+                  src="/imgs/users/scout/dc2.jpg"
+                  alt="Scout"
+                  className="rounded-full border"
+               />
             </div>
          </div>
       </div>
